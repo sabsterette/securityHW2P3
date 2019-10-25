2c6c094c72a10fd36e5b2a856e3b4d61faf96d34
sabsterette <sabrina.nicolettee@gmail.com>
2019-10-24 23:10:42 -0400

    annotated homework questions and question 4

---
+++ question3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(int argc, const char** argv, const char** envp) {
+
+	// loops from 100 to 999
+	for (int i = 100; i < 999; i++) {
+		// i know it goes from 100 to 999
+		// but i cannot figure out exactly what it is doing
+	}
+
+}
---
+++ question4.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(int argc, const char** argv, const char** envp) {
+
+	int esp1 = 7;
+	int esp2 = 100;
+	int esp3 = 0;
+
+	for (int i = 0; i < 100; i++) {
+		// from  0 to 100 it goes to 
+	}
+
+	// once out of the loop the program does more
+	// howevre I cannot get the functionality of it
+	// I do know it returns 50 at the end. 
+
+}
